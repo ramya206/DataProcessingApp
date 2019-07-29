15558149834f00bb9bf3612cd66185159a4c50e2
RamyaDharwada <ramyabhavana.dharwada@sjsu.edu>
2019-07-29 15:56:07 -0700

    Fixed minor issues

--- IoTDataMonitor/main.go
+++ IoTDataMonitor/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"os"
 )
-
+var pool = newPool()
 func main() {
 
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8005"
 	}
+
+	listenToKinesis();
+//	go pool.run()
 
 	server := NewServer()
 	server.Run(":" + port)
