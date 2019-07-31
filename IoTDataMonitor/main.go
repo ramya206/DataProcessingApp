3aba71363dfa906f3f11e7d9196efe07058ff65d
RamyaDharwada <ramyabhavana.dharwada@sjsu.edu>
2019-07-30 21:27:55 -0700

    Update main.go

--- IoTDataMonitor/main.go
+++ IoTDataMonitor/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 )
 var pool = newPool()
@@ -10,12 +11,18 @@ func main() {
 	if len(port) == 0 {
 		port = "8005"
 	}
-
+	fmt.Println("entered...Listen to Kinesis");
 	listenToKinesis();
-	go pool.run()
-	go pool.handleSender()
-	go pool.connectivityMonitor()
+	fmt.Println("entered...Running pool");
 
+	go pool.run()
+	fmt.Println("entered...starting handleSender");
+
+	go pool.handleSender()
+	fmt.Println("entered...starting connectivityMonitor");
+
+	go pool.connectivityMonitor()
+	fmt.Println("entered...starting server");
 	server := NewServer()
 	server.Run(":" + port)
 }
