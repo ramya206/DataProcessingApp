1b185524686a9c07ff97e0cdd124c4ae14314677
RamyaDharwada <ramyabhavana.dharwada@sjsu.edu>
2019-08-06 10:52:58 -0700

    Update ServerDao.go

--- IoTDataMonitor/ServerDao.go
+++ IoTDataMonitor/ServerDao.go
@@ -180,6 +180,111 @@ func getProfileById(deviceID string) interface{}{
 
 }
 
+func getAllUserProfiles() string{
+	session, err := mgo.Dial(mongodbServer)
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	glog.Info("getAllProfile: MongoDB connection success")
+	c := session.DB(mongodbDatabase).C(ProfileCollection)
+
+	type FilterBySqaud struct{
+		Type string
+		Data map[string]interface{}
+	}
+	var queryResult []map[string]interface{}
+
+	pipeline := []bson.M{bson.M{ "$group": bson.M{ "_id": "$Squad", "data": bson.M{ "$push": "$$ROOT"}}}}
+
+	err = c.Pipe(pipeline).All(&queryResult)
+	if err!= nil{
+		panic(err)
+	}
+
+	fmt.Println("getAllProfile: Data returned from DB ",queryResult)
+
+	var  i int = 0
+
+	var arrayOfSquads = make([]map[string]interface{}, len(queryResult))
+
+	for _, val := range queryResult {
+
+		squad := val["_id"]
+		fmt.Println("getAllProfile: The Squad value is  *********************",squad);
+		var squadMap = make(map[string]interface{})
+		profileArray := reflect.ValueOf(val["data"])
+
+		var arrayOfIds = make([]map[string]Profile, profileArray.Len())
+		for i:=0; i<profileArray.Len() ; i++{
+
+			arrayVal := profileArray.Index(i).Elem()
+
+			var jsonData Profile
+			if arrayVal.Kind() == reflect.Map {
+				resultMap := make(map[string]interface{})
+				for _, e := range arrayVal.MapKeys() {
+					v := arrayVal.MapIndex(e)
+
+					s:=e.Interface()
+					l:= s.(string)
+					switch t := v.Interface().(type)  {
+					case string:
+						fmt.Println(e, t)
+
+						resultMap[l] = t
+
+					default:
+						fmt.Println("not found")
+
+					}
+				}
+				fmt.Println("getAllProfile:The DEvice id from Map *********************",resultMap["DeviceId"]);
+				var devId string = resultMap["DeviceId"].(string)
+				jsonData.DeviceId  = resultMap["DeviceId"].(string)
+				jsonData.Squad = resultMap["Squad"].(string)
+				jsonData.Status = resultMap["Status"].(string)
+				jsonData.Age = resultMap["Age"].(string)
+				jsonData.Name = resultMap["Name"].(string)
+				jsonData.Image = resultMap["Image"].(string)
+				idMap := make(map[string]Profile)
+
+				idMap[devId] = jsonData
+				if idMap != nil && len(idMap) != 0 {
+					arrayOfIds[i] = idMap
+				}
+			}
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Println(" getAllProfile: Json Profile ...........",jsonData)
+		}
+		fmt.Println("getAllProfile: The Array of Ids is  *********************",arrayOfIds);
+
+		squadMap[squad.(string)] = arrayOfIds
+		if squadMap != nil && len(squadMap) != 0{
+			arrayOfSquads[i] = squadMap
+		}
+		i++;
+	}
+	type ProfileDataFormat struct{
+		Type string
+		Data []map[string]interface{}
+	}
+
+	jsonProfileData := ProfileDataFormat{
+		Type:"Profile",
+		Data : arrayOfSquads,
+
+	}
+	jsonData,err := json.Marshal(jsonProfileData)
+	glog.Info("getAllProfile: Struct data before JSon parsing", jsonProfileData)
+	return string(jsonData)
+}
+
 func getAllProfile(formatter *render.Render) http.HandlerFunc{
 	return func(w http.ResponseWriter, req *http.Request){
 
